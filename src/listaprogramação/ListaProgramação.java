752613a4c03fa1c44bbb38f66ef033e15b1def64
victorhugodias <victor.dias@al.infnet.edu.br>
2018-02-04 16:33:46 -0200

    lista completa

--- src/listaprogramação/ListaProgramação.java
+++ src/listaprogramação/ListaProgramação.java
@@ -1,101 +1,163 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package listaprogramação;
+    /*
+     * To change this license header, choose License Headers in Project Properties.
+     * To change this template file, choose Tools | Templates
+     * and open the template in the editor.
+     */
+    package listaprogramação;
 
-import java.util.Scanner;
+    import java.util.Scanner;
 
-/**
- *
- * @author victo
- */
-public class ListaProgramação {
+    /**
+     *
+     * @author victo
+     */
+    public class ListaProgramação {
 
-    public static void main(String[] args) {
-        
-        //recebendo valores digitados pelo usuario
-        Scanner entrada = new Scanner(System.in);
-        
-/*1.Faça um algoritmo que troque o valor de duas variáveis. Por exemplo, o algoritmo lê n1 igual a 3 e n2 a 17,
-    e mostra n1 igual a 17 e n2 a 3.
-        
-        int n1;
-        int n2;
-        int aux;
-        
-        System.out.println("Digite o Valor 1");
-        n1=entrada.nextInt();
-        System.out.println("Digite o Valor 2");
-        n2=entrada.nextInt();
-        System.out.println("Valor 1 digitado: " + n1);
-        System.out.println("Valor 2 digitado: " + n2);
-        aux = n1;
-        n1 = n2;
-        n2 = aux;
-        System.out.println("Valor invertido n1: " + n1);
-        System.out.println("Valor invertido n2: " + n2);*/
-        
-        
-/*2.	Faça um algoritmo para calcular a média de duas notas digitadas pelo usuário. 
+        public static void main(String[] args) {
 
-        float nota1;
-        float nota2;
-        float media;
-        
-        System.out.println("Digite a primeira nota");
-        nota1=entrada.nextFloat();
-        System.out.println("Digite a segunda nota");
-        nota2=entrada.nextFloat();
-        media = (nota1 + nota2) / 2;
-        System.out.println("A média do Aluno é : " + media);*/
-        
-/*3.Faça um algoritmo para calcular a média de duas notas digitadas pelo usuário,
-    sendo que a segunda nota tem peso dois.      
-        
-        float nota3;
-        float nota4;
-        float media1;
-        
-        System.out.println("Digite a primeira nota");
-        nota3=entrada.nextFloat();
-        System.out.println("Digite a segunda nota");
-        nota4=entrada.nextFloat();
-        media1 = (nota1 + (nota2*2)) / 3;
-        System.out.println("A média do Aluno é : " + media1);
-        */
-/* 4.	Faça um algoritmo que calcule a gorjeta a ser paga de uma conta de restaurante.
-A gorjeta é calculada como sendo 10% do valor da conta, que deve ser informado pelo usuário.
+            //recebendo valores digitados pelo usuario
+            Scanner entrada = new Scanner(System.in);
 
-        float valorConta;
-        
-        System.out.println("Digite o valor da conta");
-        valorConta=entrada.nextFloat();
-        valorConta = (valorConta * 0.1f) + valorConta;
-        System.out.println("O valor da Conta com 10% é: " + valorConta);
-*/
+    /*1.Faça um algoritmo que troque o valor de duas variáveis. Por exemplo, o algoritmo lê n1 igual a 3 e n2 a 17,
+        e mostra n1 igual a 17 e n2 a 3.
 
-/*5.	Faça um algoritmo que calcule o novo valor de um salário a partir de um valor percentual de reajuste.
-O valor atual do salário e o valor percentual do reajuste devem ser informados pelo usuário como, por exemplo, 7,3%.
+            int n1;
+            int n2;
+            int aux;
 
-        float salario;
-        float reajuste;
-        float novoSalario;
-         
-        System.out.println("Digite o valor do Salario");
-        salario=entrada.nextFloat();
-        System.out.println("Digite o valor do reajuste");
-        reajuste=entrada.nextFloat();
-        reajuste = salario * (reajuste / 100);
-        novoSalario = reajuste + salario;
-         System.out.println("O valor do reajuste é : " + reajuste);
-         System.out.println("O valor do salario com reajuste é : " + novoSalario);*/
+            System.out.println("Digite o Valor 1");
+            n1=entrada.nextInt();
+            System.out.println("Digite o Valor 2");
+            n2=entrada.nextInt();
+            System.out.println("Valor 1 digitado: " + n1);
+            System.out.println("Valor 2 digitado: " + n2);
+            aux = n1;
+            n1 = n2;
+            n2 = aux;
+            System.out.println("Valor invertido n1: " + n1);
+            System.out.println("Valor invertido n2: " + n2);*/
 
 
-        
- 
-        
-    }
+    /*2.	Faça um algoritmo para calcular a média de duas notas digitadas pelo usuário. 
+
+            float nota1;
+            float nota2;
+            float media;
+
+            System.out.println("Digite a primeira nota");
+            nota1=entrada.nextFloat();
+            System.out.println("Digite a segunda nota");
+            nota2=entrada.nextFloat();
+            media = (nota1 + nota2) / 2;
+            System.out.println("A média do Aluno é : " + media);*/
+
+    /*3.Faça um algoritmo para calcular a média de duas notas digitadas pelo usuário,
+        sendo que a segunda nota tem peso dois.      
+
+            float nota3;
+            float nota4;
+            float media1;
+
+            System.out.println("Digite a primeira nota");
+            nota3=entrada.nextFloat();
+            System.out.println("Digite a segunda nota");
+            nota4=entrada.nextFloat();
+            media1 = (nota1 + (nota2*2)) / 3;
+            System.out.println("A média do Aluno é : " + media1);
+            */
+    /* 4.	Faça um algoritmo que calcule a gorjeta a ser paga de uma conta de restaurante.
+    A gorjeta é calculada como sendo 10% do valor da conta, que deve ser informado pelo usuário.
+
+            float valorConta;
+
+            System.out.println("Digite o valor da conta");
+            valorConta=entrada.nextFloat();
+            valorConta = (valorConta * 0.1f) + valorConta;
+            System.out.println("O valor da Conta com 10% é: " + valorConta);
+    */
+
+    /*5.	Faça um algoritmo que calcule o novo valor de um salário a partir de um valor percentual de reajuste.
+    O valor atual do salário e o valor percentual do reajuste devem ser informados pelo usuário como, por exemplo, 7,3%.
+
+            float salario;
+            float reajuste;
+            float novoSalario;
+
+            System.out.println("Digite o valor do Salario");
+            salario=entrada.nextFloat();
+            System.out.println("Digite o valor do reajuste");
+            reajuste=entrada.nextFloat();
+            reajuste = salario * (reajuste / 100);
+            novoSalario = reajuste + salario;
+             System.out.println("O valor do reajuste é : " + reajuste);
+             System.out.println("O valor do salario com reajuste é : " + novoSalario);*/
+
+    /* 6.	Faça um algoritmo que calcule a área de um quadrado, sendo que o comprimento
+    do lado é informado pelo usuário. A área do quadrado é calculada elevando-se o lado ao quadrado
+
+            float area;
+            float lado;
+            System.out.println("Digite o valor do lado do quadrado");
+            lado=entrada.nextFloat();
+            area=lado*lado;
+            System.out.println("A area do quadrado é : " + area);*/
+
+    /*7.	Faça um algoritmo que calcule a área de um retângulo, sendo que os comprimentos
+    da altura e da base são informados pelo usuário. A área do retângulo é calculada
+    multiplicando-se a altura pela base.
+
+            float area;
+            float base;
+            float altura;
+            System.out.println("Digite o valor da base do retângulo");
+            base=entrada.nextFloat();
+            System.out.println("Digite o valor da altura do retângulo");
+            altura=entrada.nextFloat();
+            area = base * altura;
+            System.out.println("A area do retangulo é : " + area);*/
     
-}
+    /*8.	Faça um algoritmo que calcule a área de um círculo, sendo que o 
+    comprimento do raio é informado pelo usuário. A área do círculo é calculada 
+    multiplicando-se Pi ao raio ao quadrado.
+        
+         double area; 
+         double pi = 3.1456;
+         double raio;
+         System.out.println("Digite o raio do círculo");
+         raio=entrada.nextDouble();
+         area = pi*(raio*raio);
+         System.out.println("A area do círculo é: " + area);*/
+    
+    /*9.	Faça um algoritmo que calcule a área de uma esfera, sendo que o 
+    comprimento do raio é informado pelo usuário. A área da esfera é calculada 
+    multiplicando-se 4 vezes Pi ao raio ao quadrado.
+    
+        double area;
+        double pi = 3.1456;
+        double raio;
+        System.out.println("Digite o raio do círculo");
+        raio=entrada.nextDouble(); 
+        area = 4 * pi * (raio * raio);
+        System.out.println("A area da esfera é: " + area);*/
+    
+    /*10.	Faça um algoritmo que calcule o volume de uma caixa de água cilíndrica,
+    sendo que os comprimentos do raio e a altura são informados pelo usuário. 
+    O volume é calculado multiplicando-se Pi, ao raio ao quadrado, a altura.
+    
+        double area;
+        double pi = 3.1456;
+        double raio;
+        double altura;
+        System.out.println("Digite o raio da caixa de água");
+        raio=entrada.nextDouble();  
+        System.out.println("Digite a altura da caixa de água");
+        altura=entrada.nextDouble(); 
+        area = 2 * pi * raio * (raio + altura);
+        System.out.println("A area da esfera é: " + area);*/
+    
+    
+    
+        
+        }
+
+    }
